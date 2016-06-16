74d8732c897f26f0dfc548f96c62bfdcf4a8a611
Drew Shaw <darwin401@gmail.com>
2016-06-16 14:11:46 -0500

    project

--- project.clj
+++ project.clj
@@ -7,12 +7,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [net.mikera/core.matrix "0.36.2-SNAPSHOT"]
-                 [net.mikera/core.matrix.testing "0.0.5-SNAPSHOT"]
-                 [org.nd4j/nd4j-x86 "0.4-rc3.8"]
-                 ;[org.clojure/core.typed "0.3.23"]
-                 #_[org.nd4j/jcublas "6.5"]
-                 #_[org.nd4j/nd4j-jcublas-6.5 "0.0.3.5.5.6-SNAPSHOT"]]
+                 [net.mikera/core.matrix "0.40.0"]
+                 [net.mikera/core.matrix.testing "0.0.4"]
+                 [org.nd4j/nd4j-x86 "0.4-rc3.8"]]
 ;    :injections [(require 'clojure.core.typed)
 ;                 (clojure.core.typed/install)]
 ;  :core.typed {:check [nd4clj.core]}
