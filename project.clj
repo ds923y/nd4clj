c369c45d58da33d40333905643c6b921eacbbe59
Drew Shaw <darwin401@gmail.com>
2016-06-16 14:14:25 -0500

    project

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject nd4clj "0.1.0-SNAPSHOT"
-    :plugins [[lein-typed "0.3.5"]]
   :description "An implementation of core.matrix protocols with nd4j."
   :url "https://github.com/whilo/nd4clj"
   :source-paths      ["src/clojure"]
@@ -10,7 +9,4 @@
                  [net.mikera/core.matrix "0.40.0"]
                  [net.mikera/core.matrix.testing "0.0.4"]
                  [org.nd4j/nd4j-x86 "0.4-rc3.8"]]
-;    :injections [(require 'clojure.core.typed)
-;                 (clojure.core.typed/install)]
-;  :core.typed {:check [nd4clj.core]}
   )
