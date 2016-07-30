c159343c2c8a7d20551cce1189e1ade5875fa9e4
Drew Shaw <darwin401@gmail.com>
2016-07-30 17:23:45 -0500

    group-id added

--- project.clj
+++ project.clj
@@ -1,13 +1,9 @@
-(defproject nd4clj "0.1.0-SNAPSHOT"
+(defproject org.clojars.ds923y/nd4clj "0.1.0-SNAPSHOT"
 :plugins [[lein-cljfmt "0.5.3"]]
   :description "An implementation of core.matrix protocols with nd4j."
-  :source-paths      ["src/clojure"]
-  :java-source-paths ["src/java"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [net.mikera/core.matrix "0.40.0"]
+  :dependencies [[net.mikera/core.matrix "0.40.0"]
                  [net.mikera/core.matrix.testing "0.0.4"]
-                 [org.nd4j/nd4j-native "0.4-rc3.10"]
-                 ]
+                 [org.nd4j/nd4j-native "0.4-rc3.10"]]
   )
