171edb9af6df88a46e81e6611b0e192fca36f134
Drew Shaw <darwin401@gmail.com>
2016-08-16 02:28:36 -0500

    test setup changed

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject org.clojars.ds923y/nd4clj "0.1.0-SNAPSHOT"
-:plugins [[lein-cljfmt "0.5.3"]]
+  :plugins [[lein-cljfmt "0.5.3"]]
   :description "An implementation of core.matrix protocols with nd4j."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[net.mikera/core.matrix "0.40.0"]
-                 [net.mikera/core.matrix.testing "0.0.4"]
-                 [org.nd4j/nd4j-native "0.4-rc3.10"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 
+                 [net.mikera/core.matrix "0.53.0"]
+                 [org.nd4j/nd4j-native "0.5.0"]]
+  
+ :profiles {:dev {:dependencies [[net.mikera/core.matrix "0.53.0" :classifier "tests"]] 
+                   }} 
   )
--- test/nd4clj/core_test.clj
+++ test/nd4clj/core_test.clj
@@ -1,9 +1,9 @@
 (ns nd4clj.core-test
   (:require [clojure.pprint :refer [pprint]]
-            [clojure.core.matrix :refer [matrix mmul] :as mat]
+            [clojure.core.matrix :refer :all :as m]
             [clojure.test :refer :all]
             [clojure.core.matrix.compliance-tester :as compliance]
-            [nd4clj.kiw :refer :all])
+            [nd4clj.matrix])
   (:import [org.nd4j.linalg.factory Nd4j]))
 
 
@@ -12,5 +12,15 @@
 
 (clojure.core.matrix/set-current-implementation :nd4j)
 
+(deftest slice-tests
+  (is (equals [3 4] (slice (matrix :nd4j [[1 2] [3 4]]) 1))) ;; second slice
+  (is (equals [1 3] (slice (matrix :nd4j [[1 2] [3 4]]) 1 0))) ;; first slice of second dimension
+  ) 
+
+(deftest shape-tests
+  (is (= [2] (shape (first (slices (matrix :nd4j [[2 0] [0 2]])))))) ;; first of slice sequence
+  (is (nil? (shape (first (eseq (matrix :nd4j [[2 0] [0 2]])))))) ;; first element
+  ) 
+
 (deftest compliance-test
-  (clojure.core.matrix.compliance-tester/instance-test (mat/matrix :nd4j [[2 0] [0 2]]))) 
+  (clojure.core.matrix.compliance-tester/instance-test (matrix :nd4j [[2 0] [0 2]]))) 
