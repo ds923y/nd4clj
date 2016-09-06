5b87c5220916ee9e302460108fadd860b37de848
Drew Shaw <darwin401@gmail.com>
2016-09-06 01:53:59 -0500

    project cng

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 
+                 [com.google.guava/guava "19.0"]
                  [net.mikera/core.matrix "0.51.1-SNAPSHOT"]
                  [org.nd4j/nd4j-native "0.5.0"]]
   
